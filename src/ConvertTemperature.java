04203a9ac50e70373836a253bb116973c4a25938
kaitlynchan <ununoctium829@gmail.com>
2016-08-30 08:33:59 -0700

    First Commit!
    Converts Fahrenheit to Celcius

---
+++ src/ConvertTemperature.java
@@ -0,0 +1,16 @@
+/*Kaitlyn Chan
+ * Convert Temp Program
+ * @8/30/16
+ */
+public class ConvertTemperature {
+	
+	public static void main (String[]args){
+		
+		int Fah = 32;
+		int Cel = (Fah-32)*(5/9);
+		
+		
+		System.out.println("Temperature from Fahrenheit to Celcius: " + Cel);
+	}
+
+}
