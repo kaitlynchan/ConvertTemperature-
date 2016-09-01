5897a5c7b48a3b277d88ff52322ad65424998eb6
kaitlynchan <ununoctium829@gmail.com>
2016-09-01 08:04:37 -0700

    hi

--- src/ConvertTemperature.java
+++ src/ConvertTemperature.java
@@ -1,13 +1,14 @@
 /*Kaitlyn Chan
  * Convert Temp Program
  * @8/30/16
+ * 1st Period
  */
 public class ConvertTemperature {
 	
 	public static void main (String[]args){
 		
-		int Fah = 32;
-		int Cel = (Fah-32)*(5/9);
+		double Fah = 212.0;
+		double Cel = (Fah-32)*(5)/(9);
 		
 		
 		System.out.println("Temperature from Fahrenheit to Celcius: " + Cel);
